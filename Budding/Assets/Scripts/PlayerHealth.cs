62358c5137157572518cbc866e03a1ba0c4668de
AriShoo <al_sherlock95@outlook.com>
2023-01-11 03:43:50 +0000

    Health Script and other UI
    Added the player health to the player and began adding in the UI for menus and a health bar.

---
+++ Budding/Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealth : MonoBehaviour
+{
+    public static event Action OnPlayerDeath;
+    public float HitPoints;
+    public float MaxHitPoints = 100;
+    public HealthBarBehave HealthBar;
+    
+    void Start()
+    {
+        HitPoints = MaxHitPoints;
+        HealthBar.SetHealth(HitPoints,  MaxHitPoints);
+    }
+
+    public void  TakeDamage(float damage)
+    {
+        HitPoints -= damage;   
+
+        if (HitPoints <= 0)
+        {
+            HitPoints = 0;
+            //Display game over screen
+            OnPlayerDeath?.Invoke();
+        }
+    }
+}
